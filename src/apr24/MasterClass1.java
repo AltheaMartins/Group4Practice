3f55d3ff6f4b2eb949aeb443c7af46c7e4883400
AltheaMartins <161070940+AltheaMartins@users.noreply.github.com>
2024-04-24 10:42:12 -0700

    update

--- src/apr24/MasterClass1.java
+++ src/apr24/MasterClass1.java
@@ -1,12 +1,14 @@
 package apr24;
 
-public class AltheaClass1 {
+public class MasterClass1 {
 
     public static void main(String[] args) {
 
         System.out.println("Hello.");
 
         int num1=2;
+
+
     }
 
 }
